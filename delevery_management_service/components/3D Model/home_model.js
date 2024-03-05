c70a61b27840204ed7bf933b3a447443be63ca5d
Shreyash Sanjay Dongare <shreyash@Shreyashs-MacBook-Pro.local>
2024-03-06 01:13:43 +0530

    2 impementing 3d model hero sec..

---
+++ delevery_management_service/components/3D Model/home_model.js
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline';
+
+export default function HomeModel() {
+  return (
+    <>
+    <Spline scene="https://prod.spline.design/e-xZ7FcflKcI0jUG/scene.splinecode" />
+    </>
+  );
+}
