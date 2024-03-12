8309011feeb5d2fdb4ac7bca27254217e7b0f96c
Shreyash Sanjay Dongare <shreyash@Shreyashs-MacBook-Pro.local>
2024-03-13 00:31:43 +0530

    Lib commit

--- delevery_management_service/lib/db.js
+++ delevery_management_service/lib/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const ConnectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {});
-    console.log("Connected to MongoDB");
+    await mongoose.connect(url);
+    console.log(process.env.MONGODB_URI, {});
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1); // Consider a more graceful exit strategy for production
@@ -11,3 +11,4 @@ const ConnectDB = async () => {
 };
 
 export default ConnectDB;
+// process.env.MONGODB_URI, {}
